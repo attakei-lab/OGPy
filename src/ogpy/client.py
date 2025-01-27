f6aeb9ea30b3d8904f7ecb12875ca34c816382b1
Kazuya Takei <myself@attakei.net>
2025-01-27 23:03:43 +0900

    fix: Adjust latest params of httpx

--- src/ogpy/client.py
+++ src/ogpy/client.py
@@ -8,6 +8,6 @@ from . import types, parser
 
 def fetch(url: str) -> types.Metadata:
     """Fetch and parse HTTP content."""
-    resp = httpx.get(url, allow_redirects=True)
+    resp = httpx.get(url, follow_redirects=True)
     soup = BeautifulSoup(resp.text, "html.parser")
     return parser.parse(soup)
