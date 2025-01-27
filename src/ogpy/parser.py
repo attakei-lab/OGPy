f620b4ce39fa34c6011dcb8aed68793820f1b090
Kazuya Takei <myself@attakei.net>
2025-01-27 13:22:33 +0900

    feat: Support structured-properties of images
    - Parse 'og:image:XXX' properties.
    - Some attributes parse as integer.

--- src/ogpy/parser.py
+++ src/ogpy/parser.py
@@ -7,6 +7,12 @@ from bs4 import BeautifulSoup
 from . import types
 
 
+def parse_type(name):
+    if name in ["width", "height"]:
+        return int
+    return str
+
+
 def parse(soup: BeautifulSoup) -> types.Metadata:
     props: dict[str, Any] = {}
     if not soup.head:
@@ -17,9 +23,15 @@ def parse(soup: BeautifulSoup) -> types.Metadata:
         if not meta["property"].startswith("og:"):
             continue
         prop = meta["property"][3:]
+        # image and attributes
         if prop == "image":
             props.setdefault("images", [])
             props["images"].append(types.ImageMetadata(url=meta["content"]))
             continue
+        if prop.startswith("image:"):
+            prop = prop[6:]
+            setattr(props["images"][0], prop, parse_type(prop)(meta["content"]))
+            continue
+        # Other properties
         props[prop] = meta["content"]
     return types.Metadata(**props)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,92 @@
+import pytest
 from bs4 import BeautifulSoup
 
 from ogpy import types
 from ogpy.parser import parse
 
 
-def test_simple_content():
+@pytest.mark.parametrize(
+    "html,len_images",
+    [
+        pytest.param(
+            """
+                <html>
+                    <head>
+                        <meta property="og:title" content="EXAMPLE">
+                        <meta property="og:type" content="website">
+                        <meta property="og:url" content="http://example.com">
+                        <meta property="og:image" content="http://example.com/example.jpg">
+                    </head>
+                    <body>
+                    </body>
+                </html>
+            """,
+            1,
+            id="single-image",
+        ),
+        pytest.param(
+            """
+                <html>
+                    <head>
+                        <meta property="og:title" content="EXAMPLE">
+                        <meta property="og:type" content="website">
+                        <meta property="og:url" content="http://example.com">
+                        <meta property="og:image" content="http://example.com/example.jpg">
+                        <meta property="og:image" content="http://example.com/example.png">
+                    </head>
+                    <body>
+                    </body>
+                </html>
+            """,
+            2,
+            id="multiple-image",
+        ),
+        pytest.param(
+            """
+                <html>
+                    <head>
+                        <meta property="og:title" content="EXAMPLE">
+                        <meta property="og:type" content="website">
+                        <meta property="og:url" content="http://example.com">
+                        <meta property="og:image" content="http://example.com/example.jpg">
+                        <meta property="og:image:width" content="480">
+                    </head>
+                    <body>
+                    </body>
+                </html>
+            """,
+            1,
+            id="single-image-with-attributes",
+        ),
+        pytest.param(
+            """
+                <html>
+                    <head>
+                        <meta property="og:title" content="EXAMPLE">
+                        <meta property="og:type" content="website">
+                        <meta property="og:url" content="http://example.com">
+                        <meta property="og:image" content="http://example.com/example.jpg">
+                        <meta property="og:image" content="http://example.com/example.png">
+                        <meta property="og:image:width" content="480">
+                    </head>
+                    <body>
+                    </body>
+                </html>
+            """,
+            2,
+            id="multiple-image-with-attributes",
+        ),
+    ],
+)
+def test_simple_content(html, len_images):
+    soup = BeautifulSoup(html, "html.parser")
+    metadata = parse(soup)
+    assert isinstance(metadata, types.Metadata)
+    assert len(metadata.images) == len_images
+
+
+def test_attribute_types():
+    pass
     html = """
         <html>
             <head>
@@ -12,6 +94,9 @@ def test_simple_content():
                 <meta property="og:type" content="website">
                 <meta property="og:url" content="http://example.com">
                 <meta property="og:image" content="http://example.com/example.jpg">
+                <meta property="og:image:type" content="image/jpeg">
+                <meta property="og:image:width" content="400">
+                <meta property="og:image:height" content="300">
             </head>
             <body>
             </body>
@@ -20,3 +105,7 @@ def test_simple_content():
     soup = BeautifulSoup(html, "html.parser")
     metadata = parse(soup)
     assert isinstance(metadata, types.Metadata)
+    assert isinstance(metadata.images[0].width, int)
+    assert isinstance(metadata.images[0].height, int)
+    assert isinstance(metadata.images[0].type, str)
+    assert metadata.images[0].alt is None
